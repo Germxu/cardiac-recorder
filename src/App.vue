bac8779957946e57c5319e21b5bd8de735e10147
Finn <fengxu@tradingvisual.com>
2024-04-23 19:42:43 +0800

    add video proto sample

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,66 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, Ref, onMounted, computed } from "vue";
+const btnEl = ref<HTMLButtonElement | null>(null) as Ref<HTMLButtonElement>;
+const videoEl = ref<HTMLVideoElement | null>(null) as Ref<HTMLVideoElement>;
+const canvasEl = ref<HTMLCanvasElement | null>(null) as Ref<HTMLCanvasElement>;
+const ctx = computed(() =>
+  canvasEl.value.getContext("2d")
+) as Ref<CanvasRenderingContext2D>;
+
+const { setCtx, anim } = drawCanvas(ctx);
+
+onMounted(() => {
+  if (!btnEl.value || !videoEl.value || !canvasEl.value) {
+    console.error("Element not found");
+    return;
+  }
+
+  anim();
+  setCtx();
+  const stream = canvasEl.value.captureStream();
+  videoEl.value.srcObject = stream;
+
+  btnEl.value.onclick = enterPictureInPicture;
+});
+
+function drawCanvas(ctx: Ref<CanvasRenderingContext2D>) {
+  function setCtx() {
+    ctx.value.font = "180px Arial";
+    ctx.value.textAlign = "center";
+    ctx.value.textBaseline = "middle";
+  }
+
+  function anim() {
+    ctx.value.fillStyle = "white";
+    ctx.value.fillRect(0, 0, canvasEl.value.width, canvasEl.value.height);
+    ctx.value.fillStyle = "black";
+    ctx.value.fillText(
+      new Date().toTimeString().split(" ")[0],
+      canvasEl.value.width / 2,
+      canvasEl.value.height / 2
+    );
+    requestAnimationFrame(anim);
+  }
+  return { setCtx, anim };
+}
+function enterPictureInPicture() {
+  videoEl.value.requestPictureInPicture().then((pictureInPictureWindow) => {
+    console.log("Entered Picture-in-Picture mode.");
+
+    pictureInPictureWindow.addEventListener("resize", () => {}, false);
+  });
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <canvas id="canvas" width="800" height="480" ref="canvasEl"></canvas>
+  <video
+    id="video"
+    controls
+    muted
+    autoplay
+    ref="videoEl"
+    style="height: 0"
+  ></video>
+  <button id="btn" ref="btnEl">PiP Now</button>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
